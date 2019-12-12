b5d7437c1000e9e0932dac4973e1c2c0b259d156
rbbinder <robin.basalaev-binder@mail.mcgill.ca>
2019-12-12 14:55:52 -0500

    Import census data for CSD Charlottetown

--- R/02_import.R
+++ R/02_import.R
@@ -6,10 +6,12 @@ source("R/01_helper_functions.R")
 
 ### Build geometries ###########################################################
 
-CRM <-
+## For Charlottetown only (not PEI-wide)
+
+Charlottetown <-
   get_census(dataset = "CA16", regions = list(CSD = "1102075"), level = "CSD",
              geo_format = "sf") %>% 
-  st_transform(32617)
+  st_transform(32620)
 
 streets <- 
   (getbb("Charlottetown Prince Edward Island") * c(1.01, 0.99, 0.99, 1.01)) %>% 
@@ -21,34 +23,33 @@ CRM_streets <-
   rbind(streets$osm_polygons %>% st_cast("LINESTRING"), streets$osm_lines) %>% 
   as_tibble() %>% 
   st_as_sf() %>% 
-  st_transform(32617) %>%
-  st_intersection(CRM) %>% 
+  st_transform(32620) %>%
+  st_intersection(Charlottetown) %>% 
   select(osm_id, name, geometry)
-
-neighbourhoods <-
-  read_sf(dsn = "data", layer = "charlottetown") %>%
-  st_transform(32617) %>% 
-  select(id = OBJECTID, neighbourhood = OLD_DIST, geometry) %>% 
-  group_by(neighbourhood) %>% 
-  st_buffer(0) %>% 
-  summarize(count = n())
-
-names <- read_csv("data/names.csv")
-
-neighbourhoods <- neighbourhoods %>% 
-  left_join(names, by = "neighbourhood")
-
 
 ### Census import ##############################################################
 
-CTs_charlottetown <-
+DAs_charlottetown <-
   get_census(
-    dataset = "CA16", regions = list(PR = "11"), level = "DA",
+    dataset = "CA16", regions = list(CSD = "1102075"), level = "DA",
     vectors = c("v_CA16_2398", "v_CA16_5078", "v_CA16_4888", "v_CA16_6695",
                 "v_CA16_4837", "v_CA16_4838", "v_CA16_512", 
                 "v_CA16_3393", "v_CA16_3996"),
     geo_format = "sf") %>% 
-  st_transform(32617) %>% 
-  filter(Type == "DA") %>% 
-  select(GeoUID, PR_UID, CMA_UID, Population, Households, contains("v_CA"))
+  st_transform(32620) %>% 
+  select(GeoUID, CSD_UID, Population, Households, contains("v_CA")) %>% 
+  set_names(c("Geo_UID", "CMA_UID", "population", "households", "med_income",
+    "university_education", "housing_need", "non_mover", "owner_occupier", 
+    "rental", "official_language", "citizen", "white", "geometry")) %>% 
+  mutate_at(
+    .vars = c("university_education", "non_mover", 
+              "official_language", "citizen", "white"),
+    .funs = list(`pct_pop` = ~{. / population})) %>% 
+  mutate_at(
+    .vars = c("housing_need", "owner_occupier", "rental"),
+    .funs = list(`pct_household` = ~{. / households}))
+
+### [ OMITTED ] Import CMHC neighbourhoods #################################################
+
+
 
